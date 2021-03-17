355d6a8572868a14bc126a39550dbba74ea3ac1d
Krittawat Wisedsuwon <krittawat.wis@lightnet.io>
2021-03-17 11:01:38 +0700

    create chai setup and mod mytoken test and add new mytokensale

---
+++ test/MyTokenSale.test.js
@@ -0,0 +1,15 @@
+const Token = artifacts.require("MyToken");
+const TokenSale = artifacts.require("MyTokenSale");
+
+const chai = require("./chaisetup.js");
+const BN = web3.utils.BN;
+const expect = chai.expect;
+
+contract("TokenSale", async function(accounts) {
+    const [ initialHolder, recipient, anotherAccount ] = accounts;
+
+    it("there shouldnt be any coins in my account", async () => {
+        let instance = await Token.deployed();
+        return expect(instance.balanceOf.call(initialHolder)).to.eventually.be.a.bignumber.equal(new BN(0));
+    });
+});
