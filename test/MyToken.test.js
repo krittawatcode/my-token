5869188f3721d4afe24607b35976e00f87a2056b
Krittawat Wisedsuwon <krittawat.wis@lightnet.io>
2021-03-17 10:45:08 +0700

    add crowdsale and test

--- test/MyToken.test.js
+++ test/MyToken.test.js
@@ -14,21 +14,24 @@ const expect = chai.expect;
 contract("Token Test", async accounts => {
     const [ initialHolder, recipient, anotherAccount ] = accounts;
 
+    beforeEach(async () => {
+        this.myToken = await Token.new(1000);
+    });
 
     it("All tokens should be in my account", async () => {
-    let instance = await Token.deployed();
-    let totalSupply = await instance.totalSupply();
-    //old style:
-    //let balance = await instance.balanceOf.call(initialHolder);
-    //assert.equal(balance.valueOf(), 0, "Account 1 has a balance");
-    //condensed, easier readable style:
-    expect(instance.balanceOf(initialHolder)).to.eventually.be.a.bignumber.equal(totalSupply);
+        let instance = this.myToken;
+        let totalSupply = await instance.totalSupply();
+        //old style:
+        //let balance = await instance.balanceOf.call(initialHolder);
+        //assert.equal(balance.valueOf(), 0, "Account 1 has a balance");
+        //condensed, easier readable style:
+        expect(instance.balanceOf(initialHolder)).to.eventually.be.a.bignumber.equal(totalSupply);
     });
 
 
     it("I can send tokens from Account 1 to Account 2", async () => {
         const sendTokens = 1;
-        let instance = await Token.deployed();
+        let instance = this.myToken;
         let totalSupply = await instance.totalSupply();
         expect(instance.balanceOf(initialHolder)).to.eventually.be.a.bignumber.equal(totalSupply);
         expect(instance.transfer(recipient, sendTokens)).to.eventually.be.fulfilled;      
@@ -38,7 +41,7 @@ contract("Token Test", async accounts => {
   
   
     it("It's not possible to send more tokens than account 1 has", async () => {
-        let instance = await Token.deployed();
+        let instance = this.myToken;
         let balanceOfAccount = await instance.balanceOf(initialHolder);
   
         expect(instance.transfer(recipient, new BN(balanceOfAccount+1))).to.eventually.be.rejected;
